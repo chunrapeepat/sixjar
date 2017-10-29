56d510ad0c3f629761527d9e9490eaf8cea69f29
chunza2542 <chunza2542@hotmail.com>
2017-10-29 23:10:16 +0700

    Design navigator bar prototype

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,55 @@
 import React, {Component} from 'react'
 import styled from 'react-emotion'
+import Ink from 'react-ink'
 
 const Container = styled('div')`
   width: 100%;
   height: 100vh;
-  background: red;
+  background: #fcfcfc;
+  border: 1px solid #ccc;
+`
+
+const Padding = styled('div')`
+  padding: 15px;
+`
+
+const HeroIcon = styled('h1')`
+  margin: 0;
+  font-family: sans-serif;
+`
+
+const MenuLinkItem = styled('div')`
+  width: calc(100% - 30px);
+  font-family: sans-serif;
+  cursor: pointer;
+  padding: 15px;
+  position: relative;
+  background: ${props => (props.active) ? '#f0f0f0' : '#fefefe'};
 `
 
 export default class extends Component {
   render(){
     return (
       <Container>
-        This is a nav
+        <Padding>
+          <HeroIcon>SIXJAR</HeroIcon>
+        </Padding>
+        <MenuLinkItem active>
+          Overview
+          <Ink />
+        </MenuLinkItem>
+        <MenuLinkItem>
+          Transaction
+          <Ink />
+        </MenuLinkItem>
+        <MenuLinkItem>
+          Budget
+          <Ink />
+        </MenuLinkItem>
+        <MenuLinkItem>
+          Setting
+          <Ink />
+        </MenuLinkItem>
       </Container>
     )
   }
