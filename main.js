3b57c76c7e7de330feff9dc5f8845f9e23afcbd2
chunza2542 <chunza2542@hotmail.com>
2017-11-02 10:08:28 +0700

    Open External link in default browser

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const electron = require('electron')
+const open = require('open')
 const {app, BrowserWindow} = electron
 
 // This method will be call when Electron
@@ -6,4 +7,11 @@ const {app, BrowserWindow} = electron
 app.on('ready', () => {
   const window = new BrowserWindow({width: 1200, height: 750, resizable: false})
   window.loadURL(`http://localhost:3000`)
+
+  // Open External link in default web browser
+  const contents = window.webContents
+  contents.on('new-window', (e, uri) => {
+    e.preventDefault()
+    open(uri)
+  })
 })
